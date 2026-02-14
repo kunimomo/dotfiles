fe289f369d567cfe33af5902ceca5a72b714b1b6
kunimomo <kinishino1fuka2@outlook.jp>
2026-02-14 17:20:25 +0900

    最新化

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,35 +6,18 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  {'preservim/nerdtree'},
-  {'akinsho/toggleterm.nvim', version = "*", config = true},
-  {'junegunn/fzf'},
-  {'junegunn/fzf.vim'}
+  {
+    'junegunn/fzf.vim',
+    dependencies = { 'junegunn/fzf' }
+  }
 })
-
-require('toggleterm').setup {
-  open_mapping = [[<C-t>]],
-  hide_numbers = true,
-  shade_terminals = false,
-  float_opts = {
-    border = 'single',
-    width = 100,
-    height = 25,
-    winblend = 0,
-    highlights = {
-      border = "Normal",
-      background = "Normal",
-    },
-  },
-  direction = 'float',
-}
 
 --[[ options ]]
 local options = {
@@ -52,27 +35,22 @@ local options = {
   cursorline = true,
   -- カーソルのある列をハイライト
   cursorcolumn = true,
-  -- UTF-8でエンコーディング
-  encoding = "utf-8",
 }
 
+-- options の適用
 for key, value in pairs(options) do
   vim.opt[key] = value
 end
 
 --[[ keymap ]]
-local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
+-- silent: コマンド表示を抑制
+local opts = { silent = true }
+-- Leaderキーを Space に設定
 vim.g.mapleader = ' '
+-- 関数名を短縮
+local keymap = vim.keymap.set
 
--- keymap('i', '{', '{}', opts)
--- keymap('i', '[', '[]', opts)
 keymap('i', 'jj', '<ESC>', opts)
-
--- nerdtree
--- keymap('n', '<C-t>', ':NERDTreeToggle<CR>', opts)
-
--- fzf.vim
 keymap('n', '<Leader>f', ':Files<CR>', opts)
 keymap('n', '<Leader>g', ':GFiles<CR>', opts)
 keymap('n', '<Leader>G', ':GFiles?<CR>', opts)
